7bb17886d250ba5bf16419fe13804f794b5b3d22
xiiethan <ethan_dillinger458@yahoo.com>
2020-10-16 00:20:42 +0000

    Finished all js

--- index.js
+++ index.js
@@ -15,33 +15,24 @@ app.use(express.static("public"));
 //tell app to use Body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// BODY 
-/*
-app.get('/', function (req, res) {
-  res.render('Home', {});
-});
-app.get('/random', function (req, res) {
-  res.render('Random', {});
-});
-*/
+//This function uses the XKCD comic of the day API and returns us the comic of the day on our home page
 app.get('/', function(req, res){
     fetch('http://xkcd.com/info.0.json')
     .then(res => res.json())
     .then(data => {
-        res.render('/', {data:data})
+        res.render('home', {data:data})
     });
 });
-
+//This function uses the XKCD specific comic and uses a random number generator to give us a random comic
 app.get('/random', function(req, res){
     let randomNumber = rand(1, 600);
     fetch('http://xkcd.com/'+randomNumber+'/info.0.json')
     .then(res => res.json())
     .then(data => {
-        res.render('/random', {data:data})
+        res.render('random', {data:data})
     });
 });
-
+//Random Number Generator
 function rand(min, max) {
     let randomNum = Math.random() * (max - min) + min;
     return Math.round(randomNum);
